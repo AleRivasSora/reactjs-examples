6029250bae6cac8382fd29e07936f46d80a8a524
Alexander Rivas <123262411+AleRivasSora@users.noreply.github.com>
2023-12-20 06:18:37 -0400

    Add files via upload

---
+++ src/componentes/filtro.js
@@ -0,0 +1,89 @@
+import { useState } from "react";
+
+export const Filtrado = () => {
+    const [allPokemons, setAllPokemons] = useState([]);
+    const [filteredPokemon, setFilteredPokemon] = useState([]);
+    const [selectedTypes, setSelectedTypes] = useState([]);
+
+    const getAllPokemons = async (limit = 50) => {
+		const baseURL = 'https://pokeapi.co/api/v2/';
+
+		const res = await fetch(
+			`${baseURL}pokemon?limit=${limit}&offset=${offset}`
+		);
+		const data = await res.json();
+
+		const promises = data.results.map(async pokemon => {
+			const res = await fetch(pokemon.url);
+			const data = await res.json();
+			return data;
+		});
+		const results = await Promise.all(promises);
+
+		setAllPokemons([...allPokemons, ...results]);
+    }
+    
+    const FilterByType = (type) => {
+        setSelectedTypes(prevTypes =>
+            prevTypes.includes(type)
+            ? prevTypes.filter(t => t !== type)
+            : [...prevTypes, type]
+        );
+    }
+
+    const [typeSelected, setTypeSelected] = useState({
+		grass: false,
+		normal: false,
+		fighting: false,
+		flying: false,
+		poison: false,
+		ground: false,
+		rock: false,
+		bug: false,
+		ghost: false,
+		steel: false,
+		fire: false,
+		water: false,
+		electric: false,
+		psychic: false,
+		ice: false,
+		dragon: false,
+		dark: false,
+		fairy: false,
+		unknow: false,
+		shadow: false,
+	});
+
+
+
+    const handleCheckbox = e => {
+		setTypeSelected({
+			...typeSelected,
+			[e.target.name]: e.target.checked,
+		});
+
+		if (e.target.checked) {
+			const filteredResults = globalPokemons.filter(pokemon =>
+				pokemon.types
+					.map(type => type.type.name)
+					.includes(e.target.name)
+			);
+			setfilteredPokemons([...filteredPokemons, ...filteredResults]);
+		} else {
+			const filteredResults = filteredPokemons.filter(
+				pokemon =>
+					!pokemon.types
+						.map(type => type.type.name)
+						.includes(e.target.name)
+			);
+			setfilteredPokemons([...filteredResults]);
+		}
+	};
+
+    
+    return (
+        <div>
+            
+        </div>
+            );
+    }
